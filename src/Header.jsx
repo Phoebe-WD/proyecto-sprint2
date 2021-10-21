12044add030461fecb4d5a617dd8d9aa752effd6
Phoebe <phoebe.wilckens@gmail.com>
2021-10-20 21:02:59 -0300

    update header

--- src/Header.jsx
+++ src/Header.jsx
@@ -7,7 +7,7 @@ export default function Header(props) {
       <div className="Header-container">
         <h1>Hoteles</h1>
         <p>
-          {props.from === "" || props.to === "" ? (
+          {!props.from || !props.to ? (
             <span>En cualquier fecha.</span>
           ) : (
             `Desde el ${formatDate(props.from)}, hasta el ${formatDate(
